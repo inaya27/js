011ea860b5c3d45d7846083a82671e67fa87d255
inaya27 <inayaasif161@gmail.com>
2023-04-07 11:02:40 -0700

    website code push on github

---
+++ app.js
@@ -0,0 +1,123 @@
+// ---------------chapter 13----------------
+
+if (weight > 300 && time < 6) {
+     alert("Come to our tryout!");
+     }
+     else {
+     alert("Come to our cookout!");
+     }
+
+     if (weight > 300 && time < 6 && age > 17 && gender === "male") {
+         alert("Come to our tryout!");
+         }
+         else {
+         alert("Come to our cookout!");
+         }
+
+         if (SAT > avg || GPA > 2.5 || sport === "football") {
+             alert("Welcome to Bubba State!");
+             }
+             else {
+             alert("Have you looked into appliance repair?");
+             }
+
+             if (age > 65 || age < 21 && res === "U.S.") 
+
+                if ((age > 65 || age < 21) && res === "U.S.") 
+
+                if (age > 65 || (age < 21 && res === "U.S.")) 
+
+// --------------chapter 14--------------
+
+if ((x === y || a === b) && c === d) {
+     g = h;
+     }
+     else {
+     e = f;
+     }
+
+     if (c === d) {
+         if (x === y) {
+         g = h;
+         }
+         else if (a === b) {
+         g = h;
+         }
+         else {
+         e = f;
+         }
+         }
+         else {
+         e = f;
+         }
+
+        //  ------------------chapter 15-------------------
+
+        var city0 = "Atlanta";
+        var city1 = "Baltimore";
+        var city2 = "Chicago";
+        var city3 = "Denver";
+        var city4 = "Los Angeles";
+        var city5 = "Seattle";
+        alert("Welcome to " + city3);
+
+        var cities = ["Atlanta", "Baltimore", "Chicago", "Denver", "Los Angeles", "Seattle"];
+        alert("Welcome to " + cities[3]);
+
+    // -----------------chapter 16--------------
+
+    pets[0] = "dog";
+    pets[1] = "cat";
+    pets[2] = "bird";
+
+    pets[3] = "lizard";
+    pets[6] = "snake";
+
+    pets[3] = "lizard";
+    pets[4] = "fish";
+    pets[5] = "gerbil";
+    pets[6] = "snake";
+
+    pets.pop();pets.
+    push("fish", "ferret");
+
+    // -----------------PECENTAGE CACULATOR------------------
+
+    var maths = +prompt("enter maths marks")
+var english = +prompt("enter english marks")
+var history = +prompt("enter history marks")
+var computer_science = +prompt("enter computer_science marks")
+var biology = +prompt("enter biology marks") 
+var total_marks = 500
+var obtained_marks = maths + english + history + computer_science + biology
+var percentage = obtained_marks / total_marks * 100
+console.log(percentage);
+console.log(obtained_marks);
+
+if (percentage >= 88) {
+    console.log("Grade : A-one")
+}
+else if (percentage >= 80) {
+    console.log("Grade : A")
+}
+else if (percentage >= 66) {
+    console.log("Grade : B")
+}
+else if (percentage >= 52) {
+    console.log("Grade : C")
+}
+else if (percentage >= 38) {
+    console.log("Grade : D")
+}
+else if (percentage >= 30) {
+    console.log("Grade : E")
+}
+
+
+    
+
+
+        
+
+
+            
